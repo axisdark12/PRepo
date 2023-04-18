f6fd88cec6fe8c2cbb80e8d90fe37ce70daab52b
axisdark12 <118939678+axisdark12@users.noreply.github.com>
2023-04-17 21:31:20 -0600

    aplicando bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,20 @@
+<script setup>
+
+import BlackButton from "./components/BlackButtton.vue"
+
+
+
+</script>
+
 <template>
-  <div>
-    <h2>Generador de texto de OpenAI GPT-3</h2>
-    <form @submit.prevent="generateText">
-      <label for="prompt">Ingrese su texto de entrada:</label>
-      <textarea id="prompt" v-model="prompt"></textarea>
-      <button type="submit">Generar texto</button>
-    </form>
-    <div v-if="generatedText">
-      <h3>Texto generado:</h3>
-      <p>{{ generatedText }}</p>
-    </div>
-  </div>
+
+<BlackButtton>
+  
+</BlackButtton>
+
+
 </template>
 
-<script>
-import axios from 'axios'
+<style>
 
-export default {
-  data() {
-    return {
-      prompt: '',
-      generatedText: ''
-    }
-  },
-  methods: {
-    async generateText() {
-      const apiKey = ''
-      const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions'
-      const response = await axios.post(apiUrl, {
-        prompt: this.prompt,
-        max_tokens: 100,
-        temperature: 0.5
-      }, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      })
-      this.generatedText = response.data.choices[0].text
-    }
-  }
-}
-</script>
+</style>
---
+++ src/components/BlackButtton.vue
@@ -0,0 +1,18 @@
+<script>
+methods:
+
+
+
+</script>
+
+<template>
+
+<button @click="increment" ></button>
+
+
+
+</template>
+
+<style>
+
+</style>
